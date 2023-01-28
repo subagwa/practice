58beeb37649926a821d422e0f7a1b0f14472cd57
subagwa <122138234+subagwa@users.noreply.github.com>
2023-01-28 19:07:34 +0530

    Create shellscript.sh

---
+++ shellscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create the file "hello.txt"
+touch hello.txt
+
+# Write "Hello World" to the file
+echo "Hello World" > hello.txt
+
+# Print the contents of the file to the shell
+cat hello.txt
